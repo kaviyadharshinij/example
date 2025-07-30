22c154462a36f21d6caf9c627674e85294feb6a6
kaviyadharshinij <kaviyaj43@gmail.com>
2025-07-30 00:24:56 -0700

    for loop
    for loop file

---
+++ forloop.py
@@ -0,0 +1,37 @@
+for i in range(5):
+    print(i)
+for i in range(5):
+    print('kavi')
+for i in range(2,5):
+    print(i)
+for i in range(2,5):
+    print(i,'number')
+#Odd number
+for i in range(1,10,2):
+    print(i)
+#Even number
+for i in range(2,10,2):
+    print(i)
+#Negative number
+for i in range(-11,0):
+    print(i)
+s=input("Enter name:")
+for i in (s):
+    print(i,end="^")
+x=[1,2,3,4,5,6,7,8,9,10]
+for i in x:
+    if i%2==0:
+        print(i)
+'''#table
+x=int(input("Enter the table value:"))
+a=int(input("Enter the line:"))
+o=input("Enter the operator(+,-,*,/):")
+for i in range(1,a):
+    if(o=='+'):
+        print(i,'+',x,'=',i+x)
+    elif(o=='-'):
+        print(i,'-',x,'=',i-x)
+    elif(o=='*'):
+        print(i,'*',x,'=',i*x)
+    elif(o=='/'):
+        print(i,'/',x,'=',i/x)'''
